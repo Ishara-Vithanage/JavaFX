1c9aaa4e33bc0b90e12b6805a983d31682faf502
Ishara Harshana <isharaharshana06@gmail.com>
2025-02-05 16:46:36 +0530

    signup updated

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
+
+    public Button helloButton;
+    public VBox userName;
     @FXML
     private Label welcomeText;
 
