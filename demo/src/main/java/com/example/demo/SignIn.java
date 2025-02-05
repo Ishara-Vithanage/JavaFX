14d10edf2dd0b5d29229609a73055d6b3b9d108a
Ishara Harshana <isharaharshana06@gmail.com>
2025-02-05 17:17:31 +0530

    updated

--- demo/src/main/java/com/example/demo/SignIn.java
+++ demo/src/main/java/com/example/demo/SignIn.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class HelloController {
+public class SignIn {
 
     public Button helloButton;
     public VBox userName;
