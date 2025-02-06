548bb618e40beb5a804d7672b93fd0d42454d695
Ishara Harshana <isharaharshana06@gmail.com>
2025-02-06 10:31:40 +0530

    updated

---
+++ src/main/java/com/example/demo/SignIn.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SignIn {
+
+    public Button helloButton;
+    public VBox userName;
+    public Button signupButton;
+    public Button signinButton;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void openSignup() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("sign-up.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Sign Up");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
